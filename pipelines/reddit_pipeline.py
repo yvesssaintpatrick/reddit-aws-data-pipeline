29eea18348e81e60f6c83ce8c13fd7128c4851a3
yvesssaintpatrick <kirkpatrick01@icloud.com>
2024-07-25 16:26:21 -0600

    made some changes to the etl and pipeline files to test run the task using airflow

--- pipelines/reddit_pipeline.py
+++ pipelines/reddit_pipeline.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-from etls.reddit_etl import connect_reddit, extract_posts, transform_data, load_data_to_csv
-from utils.constants import CLIENT_ID, SECRET, OUTPUT_PATH
+from etls.reddit_etl import connect_reddit, extract_posts
+# , transform_data, load_data_to_csv
+from utils.constants import CLIENT_ID, SECRET
+# , OUTPUT_PATH
 
 
 def reddit_pipeline(file_name: str, subreddit: str, time_filter='day', limit=None):
@@ -11,9 +13,9 @@ def reddit_pipeline(file_name: str, subreddit: str, time_filter='day', limit=Non
     posts = extract_posts(instance, subreddit, time_filter, limit)
     post_df = pd.DataFrame(posts)
     # transformation
-    post_df = transform_data(post_df)
-    # loading to csv
-    file_path = f'{OUTPUT_PATH}/{file_name}.csv'
-    load_data_to_csv(post_df, file_path)
+    # post_df = transform_data(post_df)
+    # # loading to csv
+    # file_path = f'{OUTPUT_PATH}/{file_name}.csv'
+    # load_data_to_csv(post_df, file_path)
 
-    return file_path
+    # return file_path
