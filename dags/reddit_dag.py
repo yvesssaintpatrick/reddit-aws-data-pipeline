8d169ff949fbca73e0e9733752085f35b0e66c49
yvesssaintpatrick <kirkpatrick01@icloud.com>
2024-07-25 15:39:21 -0600

    mmmm

--- dags/reddit_dag.py
+++ dags/reddit_dag.py
@@ -10,9 +10,10 @@ from datetime import datetime
 def setup_imports():
     sys.path.insert(0, os.path.dirname(
         os.path.dirname(os.path.abspath(__file__))))
-    global reddit_pipeline, upload_s3_pipeline
+    global reddit_pipeline
+    # , upload_s3_pipeline
     from pipelines.reddit_pipeline import reddit_pipeline
-    from pipelines.aws_s3_pipeline import upload_s3_pipeline
+    # from pipelines.aws_s3_pipeline import upload_s3_pipeline
 
 
 # function to set up imports
@@ -47,11 +48,11 @@ extract = PythonOperator(
     dag=dag
 )
 
-# upload to s3
-upload_s3 = PythonOperator(
-    task_id='s3_upload',
-    python_callable=upload_s3_pipeline,
-    dag=dag
-)
+# # upload to s3
+# upload_s3 = PythonOperator(
+#     task_id='s3_upload',
+#     python_callable=upload_s3_pipeline,
+#     dag=dag
+# )
 
-extract >> upload_s3
+# extract >> upload_s3
