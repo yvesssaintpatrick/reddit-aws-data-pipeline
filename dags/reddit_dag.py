4b1bda8d8435883902853c26f262e54884497c7a
yvesssaintpatrick <kirkpatrick01@icloud.com>
2024-07-25 14:05:57 -0600

    creation of dag, pipeline, config and constants files

---
+++ dags/reddit_dag.py
@@ -0,0 +1,48 @@
+from pipelines.reddit_pipeline import reddit_pipeline
+from pipelines.aws_s3_pipeline import upload_s3_pipeline
+import os
+import sys
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+default_args = {
+    'owner': 'Shawn Lartey',
+    'start_date': datetime(2024, 7, 25)
+}
+
+file_postfix = datetime.now().strftime("%Y%m%d")
+
+dag = DAG(
+    dag_id='etl_reddit_pipeline',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+    tags=['reddit', 'etl', 'pipeline']
+)
+
+# extraction from reddit
+extract = PythonOperator(
+    task_id='reddit_extraction',
+    python_callable=reddit_pipeline,
+    op_kwargs={
+        'file_name': f'reddit_{file_postfix}',
+        'subreddit': 'dataengineering',
+        'time_filter': 'day',
+        'limit': 100
+    },
+    dag=dag
+)
+
+# upload to s3
+upload_s3 = PythonOperator(
+    task_id='s3_upload',
+    python_callable=upload_s3_pipeline,
+    dag=dag
+)
+
+extract >> upload_s3
