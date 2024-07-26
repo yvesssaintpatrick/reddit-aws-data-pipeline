5079340b667679535d9811647bbf63c8c7b95601
yvesssaintpatrick <kirkpatrick01@icloud.com>
2024-07-25 21:44:58 -0600

    created the transform and load functions

--- etls/reddit_etl.py
+++ etls/reddit_etl.py
@@ -29,27 +29,27 @@ def extract_posts(reddit_instance: Reddit, subreddit: str, time_filter: str, lim
 
     print(posts)
 
-    # for post in posts:
-    #     post_dict = vars(post)
-    #     post = {key: post_dict[key] for key in POST_FIELDS}
-    #     post_lists.append(post)
+    for post in posts:
+        post_dict = vars(post)
+        post = {key: post_dict[key] for key in POST_FIELDS}
+        post_lists.append(post)
 
-    # return post_lists
-
-
-# def transform_data(post_df: pd.DataFrame):
-#     post_df['created_utc'] = pd.to_datetime(post_df['created_utc'], unit='s')
-#     post_df['over_18'] = np.where((post_df['over_18'] == True), True, False)
-#     post_df['author'] = post_df['author'].astype(str)
-#     edited_mode = post_df['edited'].mode()
-#     post_df['edited'] = np.where(post_df['edited'].isin([True, False]),
-#                                  post_df['edited'], edited_mode).astype(bool)
-#     post_df['num_comments'] = post_df['num_comments'].astype(int)
-#     post_df['score'] = post_df['score'].astype(int)
-#     post_df['title'] = post_df['title'].astype(str)
-
-#     return post_df
+    return post_lists
 
 
-# def load_data_to_csv(data: pd.DataFrame, path: str):
-#     data.to_csv(path, index=False)
+def transform_data(post_df: pd.DataFrame):
+    post_df['created_utc'] = pd.to_datetime(post_df['created_utc'], unit='s')
+    post_df['over_18'] = np.where((post_df['over_18'] == True), True, False)
+    post_df['author'] = post_df['author'].astype(str)
+    edited_mode = post_df['edited'].mode()
+    post_df['edited'] = np.where(post_df['edited'].isin([True, False]),
+                                 post_df['edited'], edited_mode).astype(bool)
+    post_df['num_comments'] = post_df['num_comments'].astype(int)
+    post_df['score'] = post_df['score'].astype(int)
+    post_df['title'] = post_df['title'].astype(str)
+
+    return post_df
+
+
+def load_data_to_csv(data: pd.DataFrame, path: str):
+    data.to_csv(path, index=False)
